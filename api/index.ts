ee8c44cabd711ec2d41a03ff3837f1250ab37742
eren.emmez <eren.emmez@enuygun.com>
2023-03-26 14:19:37 +0300

    root directory & vercel conf

--- api/index.ts
+++ api/index.ts
@@ -13,6 +13,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 //Routes
 app.get('/', (req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
     res.status(200);
     res.sendFile('index.html', { root: __dirname + '/public' });
 });
